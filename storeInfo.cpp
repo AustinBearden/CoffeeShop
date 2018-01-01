dc5b09a0eeec27dd169b30345b7fbc7086ed1cea
Austin Bearden <austinb99@4jlt.com>
2018-01-01 10:39:22 -0500

    Added a few things

--- storeInfo.cpp
+++ storeInfo.cpp
@@ -4,15 +4,16 @@
 //Date Created: 12/28/17
 
 #include "storeInfo.h"
-#include <iostream>
+#include <stdlib.h>
 
 storeInfo::storeInfo(string nameOfStore, string storeHours, string address) {
 
-//map:
-Map<string, string> info = 
-{"Store Name", nameOfStore},
-{"Store Hours", storeHours},
-{"Store Address", address}
+    //map:
+    Map<string, string> info = 
+
+    {"Store Name", nameOfStore},
+    {"Store Hours", storeHours},
+    {"Store Address", address}
 
 }
 
