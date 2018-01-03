6971e196ecb35239fa41b1f2f00b1c387d29d29f
Austin Bearden <austinb99@4jlt.com>
2018-01-02 23:29:44 -0500

    Small changes

--- Records.cpp
+++ Records.cpp
@@ -4,10 +4,14 @@
 //Date Created: 12/28/17
 
 #include "Records.h"
+#include "Purchase.h"
 #include <iostream>
 
 //defualt constructor
 Records::Records() {
+
+    //create a vector
+    std::vector<int> myPurchaseVector(20); //so its says that 'myPurchaseVector' is undefined. Is this okay?
 
 }
 
